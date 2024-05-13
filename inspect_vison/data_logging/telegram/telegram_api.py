aac0476908fdfaf2ee2512ce8d24f6b41ec7a1e9
ArkadySamsonenkoWork <153271915+ArkadySamsonenkoWork@users.noreply.github.com>
2024-05-13 19:30:04 +0700

    update telegram\

--- inspect_vison/data_logging/telegram/telegram_api.py
+++ inspect_vison/data_logging/telegram/telegram_api.py
@@ -1,33 +1,48 @@
 import typing as tp
+import telebot
 
-from aiogram import Bot, Dispatcher, executor, types
+import threading
 
 from ..notification import Notificator
+
+class TelegramBot:
+    def __init__(self, token:str, notificator:Notificator):
+        self.bot = telebot.TeleBot(token=token)
+        self.notificator = notificator
+        self.users_collection = set()
+        #self.dp.middleware.setup(LoggingMiddleware())
+
+    def start(self, message) -> None:
+        user_id = message.from_user.id
+        self.users_collection.add(user_id)
+        self.bot.send_message(user_id, "I will add you in the users list")
+
+    def send_messages(self, text: str):
+        for user_id in self.users_collection:
+            self.bot.send_message(user_id, text)
+
+    def update(self, update_values: dict[str, tp.Any]):
+        self.notificator.update(update_values)
+        text = self.notificator.aware()
+        if text:
+            self.send_messages(text)
+
+    def start_polling(self):
+        self.bot.register_message_handler(self.start, commands=["start"])
+        self.bot.polling(none_stop=True, interval=0)
 
 
 class TelegramApi:
     def __init__(self, token:str, notificator:Notificator):
-        self.bot = Bot(token=token)
-        self.notificator = notificator
-        self.dp = Dispatcher(self.bot)
-        self.users_collection = set()
+        self.bot = TelegramBot(token, notificator)
 
-    async def start(self, message: types.Message) -> None:
-        user_id = message.from_user.id
-        self.users_collection.add(user_id)
-        await message.answer("I will add you in the users list")
+    def run(self) -> None:
+        thread = threading.Thread(target=self.bot.start_polling, args=())
+        thread.start()
 
-    async def send_messages(self, text: str):
-        for user_id in self.users_collection:
-            await self.bot.send_message(user_id, text)
+    def update(self, update_values: dict[str, tp.Any]):
+        self.bot.update(update_values)
 
-    async def update(self, update_values: dict[str, tp.Any]):
-        self.notificator.update(update_values)
-        text = self.notificator.aware()
-        if not text:
-            await self.send_messages(text)
 
-    async def run(self) -> None:
-        self.dp.register_message_handler(self.start, commands="start")
-        # And the run events dispatching
-        await executor.start_polling(self.dp, skip_updates=True)
+if __name__ == "__main__":
+    pass
